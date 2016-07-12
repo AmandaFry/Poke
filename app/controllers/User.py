098dabd73be1a54589c91a27ba61e853f54a8455
Amanda Fry <feamandaf@hotmail.com>
2016-07-12 12:30:25 -0700

    initial commit

---
+++ app/controllers/User.py
@@ -0,0 +1,60 @@
+from system.core.controller import *
+from flask import Flask, session, flash
+
+class User(Controller):
+    def __init__(self, action):
+        super(User, self).__init__(action)
+        self.load_model('User_Model')
+
+    def index(self):
+        #It is needed for inital page load
+        return self.load_view('index.html')
+
+    def login(self):
+        #information collected to logon
+        user_info = {
+            'email' : request.form['email'],
+            'passw' : request.form['passw']
+        }
+        #sending information to model
+        user_login = self.models['User_Model'].login_user(user_info)
+        
+        #information returned from model
+        if user_login['status']  == False:
+            #return to logon page and show errors
+            for message in user_login['errors']:
+                flash(message)
+            return self.load_view('index.html')
+        else:
+            #login and store name and id in session
+            session['id'] = user_login['user'][0]['id']
+            session['name'] = user_login['user'][0]['first_name'] + ' ' + user_login['user'][0]['last_name']
+            return redirect('/dashboard')
+ 
+
+    def register(self):
+        #information collected to register
+        user_info = {
+            'f_name' : request.form['f_name'],
+            'l_name' : request.form['l_name'],
+            'email' : request.form['email'],
+            'passw' : request.form['passw'],
+            'conf_passw' : request.form['conf_passw']
+        }
+
+        #sending information to model
+        user_register = self.models['User_Model'].register_user(user_info)
+
+        #process returned information
+        if user_register['status'] == False:
+            for message in user_register['errors']:
+                flash(message)
+            return self.load_view('index.html')
+        else:
+            flash('SUCCESFULLY REGISTERED, PLEASE LOGIN!')
+            return redirect('/')
+
+    def logout(self):
+        #clear user inforamtion from session
+        session.clear()
+        return redirect('/')
---
+++ app/models/User_Model.py
@@ -0,0 +1,99 @@
+from system.core.model import Model
+from flask import Flask, session
+import re
+
+#this is used for verifications
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9\.\+_-]+@[a-zA-Z0-9\._-]+\.[a-zA-Z]*$')
+NOSPACE_REGEX = re.compile(r'^[a-zA-Z0-9]*$')
+PW_REGEX = re.compile(r'^(?=.*?[A-Z])(?=.*?\d)[A-Za-z\d]{8,}$')
+
+class User_Model(Model):
+    def __init__(self):
+        super(User_Model, self).__init__()
+
+    def login_user(self, user_info):
+        # to collect error messages
+        errors=[]
+
+        #inital verification before connecting to database
+        if len(user_info['email']) < 2 or len (user_info['passw']) < 2:
+            errors.append("Email and/or password is too short")
+        elif not EMAIL_REGEX.match(user_info['email']):
+            errors.append("Please enter valid email format")
+        if errors:
+            return {'status' : False, 'errors' : errors}
+        else:
+            #intital verifiaction passed now searching for valid login
+            query = "SELECT * FROM users WHERE email = :email"
+            data = {'email' : user_info['email']}
+            #run query and assign to varuable 
+            loged_in_user = self.db.query_db(query,data) 
+            # print ('$' * 25)
+            # print loged_in_user
+            # print ('$' * 25)
+            
+            #verify if anything returned if not, user does not exists
+            if len(loged_in_user) == 0:
+                errors.append("User was not found, please register")
+            #user email is found, verifing the password is correct
+            elif not self.bcrypt.check_password_hash(loged_in_user[0]['password'], user_info['passw']):
+                errors.append("Password is not valid")
+                return {'status' : False, 'errors' : errors}
+            else:
+                #login was successful
+                return {'status' : True, 'user' : loged_in_user}
+
+
+    def register_user(self, user_info):
+        errors=[]
+
+        #validation prior to inserting data to db
+        if len(user_info['f_name']) < 2:
+            errors.append("First name cannot be empty")
+        elif not NOSPACE_REGEX.match(user_info['f_name']):
+            errors.append("Please enter a valid first name")
+        elif len(user_info['l_name']) < 2 :
+            errors.append("Last name cannot be empty")
+        elif not NOSPACE_REGEX.match(user_info['f_name']):
+            errors.append("Please enter a valid last name")
+        elif len(user_info['email']) < 2 :
+            errors.append("Email cannot be empty")
+        elif not EMAIL_REGEX.match(user_info['email']):
+            errors.append("Please enter a valid email format")
+        elif not PW_REGEX.match(user_info['passw']):
+            errors.append("Please enter a valid password. It must be 8 charater long, at must include least one upper case and number")
+        elif len(user_info['conf_passw']) < 2 :
+            errors.append("Confirm password cannot be empty")
+        elif not (user_info['passw'] == user_info['conf_passw']):
+            errors.append("Password and confirm password must match")   
+      
+        if errors:
+            return {"status": False, "errors": errors}
+        else:
+            #check to see if email already in use
+            query = "SELECT * FROM users WHERE email = :email"
+            data = {'email': user_info['email']}
+            email_inuse = self.db.query_db(query, data)
+
+            if email_inuse:
+                errors.append("Email account already in use")
+                return {"status": False, "errors": errors}
+            else:
+                query = "INSERT INTO users (first_name, last_name, email, password, created_at, updated_at) VALUES (:f_name, :l_name, :email, :passw, NOW(), NOW())"
+                #password needs to be converted from plain text before can be part of data
+                password = user_info['passw']
+                hashed_pw = self.bcrypt.generate_password_hash(password)
+                data = {
+                    'f_name':user_info['f_name'],
+                    'l_name':user_info['l_name'],
+                    'email':user_info['email'],
+                    'passw':hashed_pw,
+                }
+                registered_user = self.db.query_db(query, data)
+                # print ('%' * 25)
+                # print registered_user
+                # print ('%' * 25)
+                return {"status": True }
+                
+
+    
